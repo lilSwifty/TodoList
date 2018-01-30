603dda6c65949ab6f715a721fc1a84c23033e316
Manolo <manisedighi@icloud.com>
2018-01-30 13:46:36 +0100

    Initial Commit

---
+++ TodoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TodoList
+//
+//  Created by Mani Sedighi on 30/01/2018.
+//  Copyright © 2018 Mani Sedighi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
